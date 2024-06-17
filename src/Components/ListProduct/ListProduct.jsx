772295abace8d94b5f23163e4483671716098679
rumeshsanju <rumeshrajapaksha1998@gmail.com>
2024-06-17 09:46:26 +0530

    first commit

---
+++ src/Components/ListProduct/ListProduct.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react"
+import './ListProduct.css'
+import cross_icon from '../../assets/cross_icon.png'
+
+const ListProduct = () =>{
+
+    const [allproducts,setAllProduct] = useState([]);
+
+    const fetchInfo = async () =>{
+        await fetch('http://localhost:4000/allproducts')
+        .then((res) =>res.json())
+        .then((data)=>{setAllProduct(data)});
+    }
+
+    useEffect(()=>{
+        fetchInfo();
+    },[])
+
+    const remove_product = async (id) => {
+        await fetch('http://localhost:4000/removeproduct',{
+            method: 'POST',
+            headers:{
+                Accept:'application/json',
+                'Content-Type':'application/json',
+            },
+            body:JSON.stringify({id:id})
+        })
+
+        await fetchInfo();
+    }
+
+    return(
+        <div className="list-product">
+            <h1>All Product List</h1>
+            <div className="listproduct-format-main">
+                <p>Products</p>
+                <p>Title</p>
+                <p>Old Price</p>
+                <p>New Price</p>
+                <p>Category</p>
+                <p>Remove</p>
+            </div>
+            <div className="listproduct-allproducts">
+                <hr/>
+                {allproducts.map((proudct,index)=>{
+                    return <><div key={index} className="listproduct-format-main listproduct-format">
+                       <img src={proudct.image} alt="" className="listproduct-product-icon" />
+                       <p>{proudct.name}</p>
+                       <p>${proudct.old_price}</p>
+                       <p>${proudct.new_price}</p>
+                       <p>{proudct.category}</p>
+                       <img onClick={()=>{remove_product(proudct.id)}} src={cross_icon} alt="" className="listproduct-remove-icon"/>
+                    </div>
+                    <hr/>
+                    </>
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default ListProduct
